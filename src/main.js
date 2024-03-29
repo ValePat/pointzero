b58f9a48086932ce6b5b556b45f1518a82d5271f
ParterOne <valerio.patricolo@gmail.com>
2024-03-29 17:11:26 +0100

    fix screen size

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+window.onload = function() {
+    debugger
+    // Controlla se è già stato salvato un flag di ricarica
+    var reloadFlag = localStorage.getItem('reloadFlag');
+    // Se non è stato salvato alcun flag di ricarica, ricarica la pagina
+    if (!reloadFlag) {
+        // Salva un flag di ricarica nello storage locale
+        localStorage.setItem('reloadFlag', 'true');
+        // Esegui il refresh della pagina
+        window.location.reload();
+    } else {
+        // Se è già stato salvato un flag di ricarica, rimuovilo
+        localStorage.removeItem('reloadFlag');
+    }
+}
